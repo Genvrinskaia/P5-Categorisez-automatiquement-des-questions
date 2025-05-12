aab74a670bc376e411cdfb8fcd42a4dffb5a6ae6
Genvrin_Gaelle <g.genvrin@icloud.com>
2025-05-12 15:22:17 +0200

    3 chargements + interface

--- app.py
+++ app.py
@@ -16,3 +16,31 @@ mlflow.set_tracking_uri("file:///C:/envs/mlflow_P5/mlruns")
 
 # ─── Chargements ──────────────────────────────────────────────────────────────
 st.write(f"Environnement Python actif : {os.environ.get('VIRTUAL_ENV', sys.prefix)}")
+
+
+# ─── Chargements ──────────────────────────────────────────────────────────────
+st.write(f"Environnement Python actif : {os.environ.get('VIRTUAL_ENV', sys.prefix)}")
+
+# 1. USE (Universal Sentence Encoder)
+@st.cache_resource
+def load_use_model():
+    return hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+use_model = load_use_model()
+
+# 2. Chargement du modèle de classification depuis MLflow
+model_dir = r"C:/envs/P5_models/USE_RL2_model"
+model = joblib.load(os.path.join(model_dir, "model.joblib"))
+
+# 3. Chargement du scaler pour normaliser les embeddings
+scaler_USE = joblib.load(r"C:\envs\P5\scaler_USE.joblib")
+
+# 4. MultiLabelBinarizer
+mlb = joblib.load(r"C:\envs\P5\P5_mlb.pkl")
+tags_list = mlb.classes_
+
+# ─── Interface ────────────────────────────────────────────────────────────────
+st.title("Gaëlle_Genvrin_P5_API – Deep Learning + USE")
+st.write("Entrez votre question StackOverflow ci-dessous 👇")
+
+question = st.text_area("Question StackOverflow", height=150)
